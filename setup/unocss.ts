c561e9e768628304e7917e34fe17851fedbd658d
Robin <git+r@obin.ch>
2023-09-18 12:41:59 +0000

    chore: migrate to unocss

--- setup/unocss.ts
+++ setup/unocss.ts
@@ -1,7 +1,7 @@
 import { defineUnoSetup } from '@slidev/types'
 
 export default defineUnoSetup(() => ({
-  shortcuts: {
-    'bg-main': 'bg-white text-[#181818] dark:bg-[#121212] dark:text-[#ddd]',
-  },
+  shortcuts: [
+    {'bg-main': 'bg-white text-[#181818] dark:bg-[#121212] dark:text-[#ddd]'}
+  ]
 }))
