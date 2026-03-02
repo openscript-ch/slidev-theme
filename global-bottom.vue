098abdf32b64c6dc3c5ea8955666a37abde40c0c
Robin <git+r@obin.ch>
2026-03-02 14:53:07 +0000

    fix: export slide numbering

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import { useSlideContext } from '@slidev/client'
+
+const { $nav } = useSlideContext()
+</script>
+
 <template>
   <footer style="font-size: 0.5em" class="absolute top-7 right-4 pr-8 right-0 text-right text-gray-400 flex z-1">
     <div>
-      <SlideCurrentNo />&nbsp;/&nbsp;<SlidesTotal />
+      {{ $nav.currentPage }}&nbsp;/&nbsp;{{ $nav.total }}
     </div>
   </footer>
 </template>
